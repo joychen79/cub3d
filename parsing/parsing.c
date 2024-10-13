0c92faf35816dfeb6ca4c57008a7290270ab0fd0
Jingjing Chen <jingchen@c2r12s4.42madrid.com>
2024-10-13 13:59:37 +0200

    2024101304

--- parsing/parsing.c
+++ parsing/parsing.c
@@ -6,9 +6,45 @@
 /*   By: jingchen <jingchen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 13:29:54 by jingchen          #+#    #+#             */
-/*   Updated: 2024/10/05 13:59:18 by jingchen         ###   ########.fr       */
+/*   Updated: 2024/10/13 13:48:57 by jingchen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+static int	check_chars(t_game *game, char value, int x, int y)
+{
+	if (value != '1' && value != '0' && value != 'N'
+		&& value != 'S' && value != 'E' && value != 'W' && value != ' ')
+		print_error("Invalid value found in map");
+	if (value == 'N' || value == 'S' || value == 'E' || value == 'W')
+	{
+		game->player.y = (x * WALL_SIZE) + WALL_SIZE / 2;
+		game->cam.grid_y = y;
+		game->player.x = (y * WALL_SIZE) + WALL_SIZE / 2;
+		game->cam.grid_x = x;
+		return (1);
+	}
+	return (0);
+}
+
+static void	check_map_chars(t_game *game, char **map)
+{
+	int	x;
+	int	y;
+	int	player_flag;
+
+	y = -1;
+	player_flag = 0;
+	while (map[++y])
+	{
+		x = -1;
+		while (map[y][++x])
+			player_flag += check_chars(game, map[y][x], x, y);
+	}
+	if (player_flag > 1)
+		print_error("Mutiple players found");
+	if (!player_flag)
+		print_error("No player position found");
+}
 
 void	parsing(char **argv, t_game *game)
 {
--- parsing/parsing.h
+++ parsing/parsing.h
@@ -6,7 +6,7 @@
 /*   By: jingchen <jingchen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 13:20:47 by jingchen          #+#    #+#             */
-/*   Updated: 2024/10/13 11:27:03 by jingchen         ###   ########.fr       */
+/*   Updated: 2024/10/13 13:57:46 by jingchen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@
 
 void    print_errors(char *errors);
 char	**read_map(char **argv);
+void    check_closed_walls(char **map);
 
 enum e_values
 {
@@ -40,5 +41,112 @@ typedef struct s_color
 	int	blue;
 	int	alpha;
 }	t_color;
+
+/*typedef struct s_vector
+{
+	double	x;
+	double	y;
+}	t_vector;
+
+typedef struct s_img
+{
+	void	*img;
+	char	*addr;
+	int		bpp;
+	int		line_len;
+	int		endian;
+	int		height;
+	int		width;
+}	t_img;
+
+typedef struct s_map_data
+{
+	char	*texture_no;
+	char	*texture_so;
+	char	*texture_we;
+	char	*texture_ea;
+	int		max_x;
+	int		max_y;
+	t_color	floor;
+	t_color	ceiling;
+}	t_map_data;
+
+enum e_keys
+{
+	W = 13,
+	A = 0,
+	S = 1,
+	D = 2,
+	UP = 126,
+	LEFT = 123,
+	DOWN = 125,
+	RIGHT = 124,
+	ESC = 53
+};
+
+/**
+ * Player position and direction
+*/
+/*typedef struct s_player
+{
+	float		x;
+	float		y;
+	float		old_x;
+	float		old_y;
+	float		camera;
+	t_vector	dir;
+}	t_player;
+
+typedef struct s_camera
+{
+	int			hit;
+	int			offset;
+	int			grid_x;
+	int			grid_y;
+	int			**buffer;
+	float		x;
+	float		y;
+	float		stepx;
+	float		stepy;
+	float		camerax;
+	float		directionx;
+	float		directiony;
+	t_vector	plane;
+	float		raydirx;
+	float		raydiry;
+	float		dx;
+	float		dy;
+	float		sidedx;
+	float		sidedy;
+	float		distance;
+	double		wallx;
+	double		wally;
+	double		textx;
+	double		texty;
+	float		lineheight;
+	double		increase;
+	double		textpos;
+}	t_camera;
+
+typedef struct s_texture
+{
+	t_img	img;
+	int		*text_value;
+}	t_texture;
+
+typedef struct s_game
+{
+	void		*mlx;
+	void		*win;
+	char		**map;
+	t_map_data	map_data;
+	t_img		img;
+	t_player	player;
+	t_camera	cam;
+	t_texture	textu_n;
+	t_texture	textu_s;
+	t_texture	textu_e;
+	t_texture	textu_w;
+}	t_game;*/
 
 #endif
